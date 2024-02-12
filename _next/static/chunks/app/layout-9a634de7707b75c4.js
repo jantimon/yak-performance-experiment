b65c00aa123dbd0495c0ebe53efad5bb4cc8d3a6
Jan Nicklas <j.nicklas@me.com>
2024-02-12 10:45:29 +0100

    first commit

---
+++ _next/static/chunks/app/layout-9a634de7707b75c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4750:function(){}},function(n){n.O(0,[462,514,744],function(){return n(n.s=4750)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/_error-fed3fa0ef65c4eb1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{7551:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(7219)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=7551)}),_N_E=n.O()}]);
